108109a670d9a6c8d4380258768c8e8b656b206a
davidtan <davidtan@thoughtworks.com>
2018-05-17 07:52:12 +0800

    Remove unused shell scripts

--- infrastructure/shell/configure_api_key.sh
+++ infrastructure/shell/configure_api_key.sh
@@ -4,7 +4,7 @@ set -e
 
 # 1. Generate your credentials on GCP. Instructions: https://cloud.google.com/iam/docs/creating-managing-service-account-keys
 # 2. Uncomment the following line:
-path_to_credentials="/app/gcp_ml_ci_cd_demo.json"
+path_to_credentials="./gcp_ml_ci_cd_demo.json"
 
 if [[ -z ${path_to_credentials+x} ]]; then
   echo "Error: please configure your api keys in ./infrastructure/shell/configure_api_key.sh"
