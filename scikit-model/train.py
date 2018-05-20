34421b188018fb057baecc9f01869f68f7b27283
davidtan <davidtan@thoughtworks.com>
2018-05-20 23:01:54 +0800

    Add job to train scikit model with ability to parse JSON input during prediction time

--- scikit-model/train.py
+++ scikit-model/train.py
@@ -1,4 +1,3 @@
-# import googleapiclient.discovery
 import json
 import numpy as np
 import os
@@ -6,12 +5,12 @@ import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
-from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import SelectKBest
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelBinarizer
 
-
-#### Preprocess data ####
-# Define the format of your input data including unused columns.
-# (These are the columns from the census data files)
+# Define the format of your input data including unused columns (These are the columns from the census data files)
 COLUMNS = (
     'age',
     'workclass',
@@ -30,8 +29,7 @@ COLUMNS = (
     'income-level'
 )
 
-# Categorical columns are columns that need to be turned into a numerical
-# value to be used by scikit-learn
+# Categorical columns are columns that need to be turned into a numerical value to be used by scikit-learn
 CATEGORICAL_COLUMNS = (
     'workclass',
     'education',
@@ -43,39 +41,89 @@ CATEGORICAL_COLUMNS = (
     'native-country'
 )
 
+
 # Load the training census dataset
 with open('./data/adult.data', 'r') as train_data:
     raw_training_data = pd.read_csv(train_data, header=None, names=COLUMNS)
+
 # Remove the column we are trying to predict ('income-level') from our features list
-train_features = raw_training_data.drop('income-level', axis=1)
-# Create our training labels list
-train_labels = (raw_training_data['income-level'] == ' >50K')
+# Convert the Dataframe to a lists of lists
+train_features = raw_training_data.drop('income-level', axis=1).as_matrix().tolist()
+# Create our training labels list, convert the Dataframe to a lists of lists
+train_labels = (raw_training_data['income-level'] == ' >50K').as_matrix().tolist()
+
 
 # Load the test census dataset
 with open('./data/adult.test', 'r') as test_data:
     raw_testing_data = pd.read_csv(test_data, names=COLUMNS, skiprows=1)
 # Remove the column we are trying to predict ('income-level') from our features list
-test_features = raw_testing_data.drop('income-level', axis=1)
-# Create our training labels list
-test_labels = (raw_testing_data['income-level'] == ' >50K.')
+# Convert the Dataframe to a lists of lists
+test_features = raw_testing_data.drop('income-level', axis=1).as_matrix().tolist()
+# Create our training labels list, convert the Dataframe to a lists of lists
+test_labels = (raw_testing_data['income-level'] == ' >50K.').as_matrix().tolist()
 
-# Convert the categorical columns to a numerical value in both the
-# training and testing dataset
-encoders = {col:LabelEncoder() for col in CATEGORICAL_COLUMNS}
 
-for col in CATEGORICAL_COLUMNS:
-    train_features[col] = encoders[col].fit_transform(train_features[col])
-    test_features[col] = encoders[col].fit_transform(test_features[col])
-  
+# Since the census data set has categorical features, we need to convert
+# them to numerical values. We'll use a list of pipelines to convert each
+# categorical column and then use FeatureUnion to combine them before calling
+# the RandomForestClassifier.
+categorical_pipelines = []
 
-#### Train model ####
-# Create and train a classifier
+# Each categorical column needs to be extracted individually and converted to a numerical value.
+# To do this, each categorical column will use a pipeline that extracts one feature column via
+# SelectKBest(k=1) and a LabelBinarizer() to convert the categorical value to a numerical one.
+# A scores array (created below) will select and extract the feature column. The scores array is
+# created by iterating over the COLUMNS and checking if it is a CATEGORICAL_COLUMN.
+for i, col in enumerate(COLUMNS[:-1]):
+    if col in CATEGORICAL_COLUMNS:
+        # Create a scores array to get the individual categorical column.
+        # Example:
+        #  data = [39, 'State-gov', 77516, 'Bachelors', 13, 'Never-married', 'Adm-clerical', 
+        #         'Not-in-family', 'White', 'Male', 2174, 0, 40, 'United-States']
+        #  scores = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        #
+        # Returns: [['Sate-gov']]
+        scores = []
+        # Build the scores array
+        for j in range(len(COLUMNS[:-1])):
+            if i == j: # This column is the categorical column we want to extract.
+                scores.append(1) # Set to 1 to select this column
+            else: # Every other column should be ignored.
+                scores.append(0)
+        skb = SelectKBest(k=1)
+        skb.scores_ = scores
+        # Convert the categorical column to a numerical value
+        lbn = LabelBinarizer()
+        r = skb.transform(train_features)
+        lbn.fit(r)
+        # Create the pipeline to extract the categorical feature
+        categorical_pipelines.append(
+            ('categorical-{}'.format(i), Pipeline([
+                ('SKB-{}'.format(i), skb),
+                ('LBN-{}'.format(i), lbn)])))
+
+# Create pipeline to extract the numerical features
+skb = SelectKBest(k=6)
+# From COLUMNS use the features that are numerical
+skb.scores_ = [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0]
+categorical_pipelines.append(('numerical', skb))
+
+# Combine all the features using FeatureUnion
+preprocess = FeatureUnion(categorical_pipelines)
+
+# Create the classifier
 classifier = RandomForestClassifier()
-classifier.fit(train_features, train_labels)
 
-# Export the classifier to a file
-pickle.dump(classifier, open('model.pkl', 'wb'))
+# Transform the features and fit them to the classifier
+classifier.fit(preprocess.transform(train_features), train_labels)
 
-# Generate 5 sample test data points
-with open('test_data.json', 'w') as outfile:
-  json.dump(test_features.as_matrix().tolist()[0:5], outfile)
+# Create the overall model as a single pipeline
+pipeline = Pipeline([
+    ('union', preprocess),
+    ('classifier', classifier)
+])
+
+# Export the model to a file
+pickle.dump(pipeline, open('./build/model.pkl', 'wb'))
+
+print('Model trained and saved')
