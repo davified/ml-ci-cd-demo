f181a5f21bbaa9e024c9b32da6e8deea03800cdc
davidtan <davidtan@thoughtworks.com>
2018-05-20 23:03:07 +0800

    Add shell scripts to deploy scikit-learn model

--- scikit-model/common.sh
+++ scikit-model/common.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export GOOGLE_APPLICATION_CREDENTIALS="~/.ssh/training-data-analyst.json"
+export GOOGLE_APPLICATION_CREDENTIALS="./gcp_ml_ci_cd_demo.json"
 export REGION="us-central1" # set to the same region where we're running Cloud ML Engine jobs
-export PROJECT_ID="training-data-analyst-202307"
+export PROJECT_ID="ml-ci-cd-demo"
 export BUCKET=${PROJECT_ID}-mlengine
-export MODEL_NAME="census_scikit_learn"
-export VERSION_NAME="v5"
+export MODEL_NAME="census_sklearn_pipeline"
+export VERSION_NAME="v4"
+
+echo "INFO: Setting project to ${PROJECT_ID}"
+gcloud config set project $PROJECT_ID
--- scikit-model/deploy.sh
+++ scikit-model/deploy.sh
@@ -4,29 +4,25 @@ set -e
 
 source ./common.sh
 
-echo "INFO: Setting project to ${PROJECT_ID}"
-gcloud config set project ${PROJECT_ID}
-
-if ! gsutil ls | grep -q gs://${BUCKET}/; then
-  gsutil mb -l ${REGION} gs://${BUCKET}
-fi
+# if ! gsutil ls | grep -q gs://${BUCKET}/; then
+#   gsutil mb -l ${REGION} gs://${BUCKET}
+# fi
 
 echo "INFO: Copying model binaries to bucket"
-gsutil cp ./model.pkl gs://$BUCKET/model.pkl
+# gsutil cp ./build/model.pkl gs://$BUCKET/model.pkl
 
 echo "INFO: Creating model resource (i.e. a container for all versions of this model)" 
-# gcloud beta ml-engine models create ${MODEL_NAME}  # note: this command is not idempotent. can only be run once
-#--region ${REGION}
+# gcloud beta ml-engine models create ${MODEL_NAME} --regions ${REGION}  # note: this command is not idempotent. can only be run once
+
 
 echo "INFO: Deploying model to GCP ML Engine"
-
 DEPLOYMENT_SOURCE="gs://$BUCKET"
 FRAMEWORK="SCIKIT_LEARN"
 
 echo "INFO: Creating a version of the model"
 gcloud beta ml-engine versions create $VERSION_NAME \
     --model $MODEL_NAME --origin $DEPLOYMENT_SOURCE \
-    --runtime-version="1.5" --framework $FRAMEWORK \
+    --runtime-version=1.5 --framework $FRAMEWORK \
     --python-version=3.5
 
 echo "INFO: Describe version"
