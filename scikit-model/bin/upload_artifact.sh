c1c724c5d5942bdde38b01f66f20935d3213660b
davidtan <davidtan@thoughtworks.com>
2018-05-22 15:53:49 +0800

    Debugging CI - finding path to gcp credentials json file

--- scikit-model/bin/upload_artifact.sh
+++ scikit-model/bin/upload_artifact.sh
@@ -6,6 +6,9 @@ current_directory="$( cd "$(dirname "$0")" ; pwd -P )"
 source ${current_directory}/common.sh
 exit_if_not_ci
 
+echo "GCP credentials are here:"
+ls -la $GOOGLE_APPLICATION_CREDENTIALS
+
 if ! gsutil ls | grep -q gs://${BUCKET}/; then
   gsutil mb -l ${REGION} gs://${BUCKET}
 fi
