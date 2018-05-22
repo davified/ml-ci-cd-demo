fdd927c8f4406089f5c02e53ec1c7b47b7d937ea
davidtan <davidtan@thoughtworks.com>
2018-05-22 17:20:57 +0800

    Remove unused shell scripts

--- infrastructure/shell/create_bq_table.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-PROJECT_ID="ml-ci-cd-demo"
-# DATASET="TO_BE_NAMED"
-# TABLE="TO_BE_NAMED"
-
-bq mk ${DATASET}
-bq mk --table ${PROJECT_ID}:${DATASET}.${TABLE}
--- infrastructure/shell/set_project_config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-source ./common.sh
-
-echo "setting gcloud cli core/project property to: ${project_id}"
-gcloud config set project ${project_id}
