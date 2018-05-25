76ed54369b22203bd90c827406a3cc7af1e8c61a
davidtan <davidtan@thoughtworks.com>
2018-05-25 12:50:10 +0800

    Explicitly activate virtual environment before running tensorflow training job

--- tf-estimator/bin/train_locally.sh
+++ tf-estimator/bin/train_locally.sh
@@ -15,6 +15,8 @@ touch $output_dir/log.txt
 
 cd ${tf_estimator_dir}
 
+source activate ml-ci-cd-demo
+
 echo "[INFO] Training tensorflow model locally"
 gcloud ml-engine local train  --package-path trainer \
                               --module-name trainer.task \
