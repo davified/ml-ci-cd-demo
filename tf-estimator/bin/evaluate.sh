4e2b8bdcc9fe2202d0e85df399030c8e19b46312
davidtan <davidtan@thoughtworks.com>
2018-05-25 11:10:28 +0800

    Debugging tf-estimator/bin/evaluate.sh

--- tf-estimator/bin/evaluate.sh
+++ tf-estimator/bin/evaluate.sh
@@ -3,7 +3,14 @@
 set -e
 
 current_directory="$( cd "$(dirname "$0")" ; pwd -P )"
-output_dir="${current_directory}/../output"
+echo "current_directory: ${current_directory}"
+echo "contents of current_directory:"
+ls ${current_directory}
+
+output_dir="${output_dir}/../output"
+echo "output_dir: ${output_dir}"
+echo "contents of output_dir:"
+ls ${output_dir}
 
 AUC_PRECISION_RECALL_THRESHOLD=$(cat ${current_directory}/AUC_PRECISION_RECALL_THRESHOLD.txt)
 auc_precision_recall_score=$(cat $output_dir/log.txt | sed -n 's/^.*auc_precision_recall\s=\s\(0\.[0-9]\+\).*$/\1/p')
