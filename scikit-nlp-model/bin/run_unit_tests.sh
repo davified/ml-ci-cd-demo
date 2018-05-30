dbbbc78c6c26ad5cb34543d9a47cd9c9d53ac672
davified <davified@gmail.com>
2018-05-30 15:44:23 +0800

    Ignore model evaluation tests in unit tests

--- scikit-nlp-model/bin/run_unit_tests.sh
+++ scikit-nlp-model/bin/run_unit_tests.sh
@@ -4,4 +4,4 @@ set -e
 
 current_directory="$( cd "$(dirname "$0")" ; pwd -P )"
 
-nosetests -w "${current_directory}/../tests" -a '!statistical_test'
+nosetests -w "${current_directory}/../tests" -I 'test_model_evaluation'
