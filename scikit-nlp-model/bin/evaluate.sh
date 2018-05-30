24370cbbe3d6d7a14f9bbeb0ab7fff9985d34a23
davified <davified@gmail.com>
2018-05-30 16:36:46 +0800

    Run tests from the correct folder

--- scikit-nlp-model/bin/evaluate.sh
+++ scikit-nlp-model/bin/evaluate.sh
@@ -4,5 +4,8 @@ set -e
 
 current_directory="$( cd "$(dirname "$0")" ; pwd -P )"
 
+echo "[DEBUG] Listing python tests directory"
+ls "$current_directory/../tests"
+
 echo "[INFO] Running model evaluation tests"
-nosetests -w "./tests" -a 'statistical_test'
+nosetests -w "$current_directory/../tests" -a 'statistical_test'
