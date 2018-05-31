4407596b277fe5c602a1a251a3040d8a59c26cf2
davified <davified@gmail.com>
2018-05-31 10:05:56 +0800

    Clean up deploy-to-staging script

--- scikit-nlp-model/bin/deploy_to_staging.sh
+++ scikit-nlp-model/bin/deploy_to_staging.sh
@@ -7,17 +7,16 @@ source ${current_directory}/common.sh
 project_directory="$(echo $current_directory | sed 's/\/ml-ci-cd-demo.*/\/ml-ci-cd-demo/g')"
 exit_if_not_ci
 
+DEPLOYMENT_SOURCE="${BUCKET}/nlp_sentiment/build"
+FRAMEWORK="SCIKIT_LEARN"
+latest_version_plus_one=$(get_next_version_name_for ${MODEL_NAME})
+echo "[INFO] Deploying model to GCP ML Engine..."
+
 model_status_code=$(gcloud beta ml-engine models list | grep -c ${MODEL_NAME} || true) # 1 if model exists, 0 otherwise
 if [[ ${model_status_code} == 0 ]]; then
     echo "[INFO] Creating model resource (i.e. a container for all versions of this model)" 
     gcloud beta ml-engine models create ${MODEL_NAME} --regions ${REGION}
 fi
-
-echo "[INFO] Deploying model to GCP ML Engine"
-DEPLOYMENT_SOURCE="${BUCKET}/nlp_sentiment/build"
-FRAMEWORK="SCIKIT_LEARN"
-
-latest_version_plus_one=$(get_next_version_name_for ${MODEL_NAME})
 
 echo "[INFO] Creating next version (${latest_version_plus_one}) for model ${MODEL_NAME}"
 gcloud beta ml-engine versions create "${latest_version_plus_one}" \
