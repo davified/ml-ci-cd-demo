84165b34eaa7031aba171eb00211e238d3636a0b
davified <davified@gmail.com>
2018-06-07 16:07:22 +0800

    Update unit test for data loader

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -8,6 +8,11 @@ mkdir -p ./data
 
 curl https://storage.googleapis.com/ml-ci-cd-demo-mlengine/nlp_sentiment/data/review_400000_samples.json -o ./data/review_400000_samples.json
 
+NO_OF_SAMPLES=10
+FIXTURES_DIR=./scikit-nlp-model/tests/fixtures
+
+head -n ${NO_OF_SAMPLES} ./data/review_400000_samples.json > ${FIXTURES_DIR}/review_${NO_OF_SAMPLES}_samples.json
+
 # echo "[INFO] Generating fixtures for unit tests from training/testing data"
 # head -n 10 ./data/adult.data > ./scikit-model/tests/fixtures/samples.csv
 
--- scikit-nlp-model/tests/test_data_loader.py
+++ scikit-nlp-model/tests/test_data_loader.py
@@ -1,30 +1,19 @@
 import os, unittest
-# import numpy as np
-# import pandas as pd
-# from constants import COLUMNS
-# from utils import load_data
-
-# class TestUtils(unittest.TestCase):
-#   def setUp(self):
-#     current_dir  = os.path.dirname(os.path.abspath(__file__))
-#     self.test_data = os.path.join(current_dir, './fixtures/samples.csv')
-#     self.test_df = pd.read_csv(self.test_data, header=None, names=COLUMNS)
-
-#     self.NO_OF_EXAMPLES = self.test_df.shape[0]
-#     self.NO_OF_FEATURES = self.test_df.shape[1] - 1
-
-#   def test_should_load_data_and_return_features_labels(self):
-#     features, labels = load_data(data_filepath=self.test_data, y_column='income-level', y_class_1=' >50K')
-
-#     self.assertEqual(np.array(features).shape, (self.NO_OF_EXAMPLES, self.NO_OF_FEATURES))
-#     self.assertEqual(np.array(labels).shape, (self.NO_OF_EXAMPLES,))
-
-#     self.assertEqual(features, self.test_df.drop('income-level', axis=1).as_matrix().tolist())
-#     self.assertTrue(all(label == 0 or label == 1 for label in labels))
-
-
-    
+from data_loader import get_data_and_labels
 
 class TestDataLoader(unittest.TestCase):
   def test_should_load_data_and_return_features_labels(self):
-    self.assertEqual(1,1)
+    current_dir  = os.path.dirname(os.path.abspath(__file__))
+    test_data_path = '{}/fixtures/review_10_samples.json'.format(current_dir)
+    with open(test_data_path) as file:
+      expected_number_of_examples = sum(1 for line in file)
+
+    texts, labels, _ = get_data_and_labels(test_data_path)
+    
+    self.assertEqual(len(texts), expected_number_of_examples)
+    self.assertEqual(len(labels), expected_number_of_examples)
+    
+    [self.assertTrue(label == 0 or label == 1) for label in labels]
+    [self.assertTrue(isinstance(text, str)) for text in texts]
+    
+
